3437816489e2f19b4d588e5a2d66da3952ead868
Ekkarat Surasit <s6502041520153@email.kmutnb.ac.th>
2024-03-28 00:53:15 +0700

    add 404 Not Found Page

--- client/distar-project/src/components/NotFoundPage/NotFoundPage.jsx
+++ client/distar-project/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import './not-found-style.css';
-import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied'; 
 const NotFoundPage = () => {
     return (
         <Container className='page-container d-flex align-items-center justify-content-center'>
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
                     <Col className='title-header'>
                         <div>
                             {/* <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXg1YTRiaGlwZjAza3Z4c2EwZW5rOHZyZ2VkMGU5czg2ZTQ5dHVtaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H54feNXf6i4eAQubud/giphy.gif" alt="My GIF" /> */}
-                            
+                            <SentimentVeryDissatisfiedIcon sx={{fontSize: '120px'}} />
                         </div>
                     </Col>
                 </Row>
